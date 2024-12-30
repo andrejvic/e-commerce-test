fd4dbd6d0a9914d3c95a50a539cfb8d57a2f95e8
andrejvic <vicanovicandrej2@gmail.com>
2024-12-31 00:42:26 +0100

    Fix 404 issue on refresh, clean up comments and debugging code, secure credentials, ensure PSR-4 compliance, and properly use autoloader.

--- frontend/src/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://andrejvicanovic.com/api/public/graphql", // Zameni sa svojim backend URL-om
-  cache: new InMemoryCache(),
-});
-
-export default client;
--- frontend/src/hoc/withParams.jsx
+++
@@ -1,9 +0,0 @@
-// withParams.js
-import { useParams } from "react-router-dom";
-
-export function withParams(Component) {
-  return (props) => {
-    const params = useParams();
-    return <Component {...props} params={params} />;
-  };
-}
